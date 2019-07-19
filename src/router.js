d6587181f34632209d3cd4fbf793e0436caaff75
Renata Macedo <macedorenata.developer@gmail.com>
2019-07-18 22:43:51 -0300

    Arquivo indesejado Please enter the commit message for your changes. Lines starting

--- src/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-const UserController = require('./controllers/UserController');
-
-routes.get('/users', UserController.users);
-
-module.exports = routes;
